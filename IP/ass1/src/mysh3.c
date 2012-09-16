b1e13d7cafb61c7fbd0982a7bdde48fa3e2f721f
Jayke Meijer <jayke.meijer@gmail.com>
2012-09-16 22:40:21 +0200

    Fixed mysh3.

--- IP/ass1/src/mysh3.c
+++ IP/ass1/src/mysh3.c
@@ -16,7 +16,7 @@ void handle_input(char input[]) {
     pid_t pid;
     char clean_input[128];
     char *input_array[64];
-    char *temp;
+    char *temp;		
     char buffer[128];
     int i = 0;
 
@@ -42,7 +42,7 @@ void handle_input(char input[]) {
  */
 void handle_double_input(char input[]) {
     char *temp, *commands[2];
-    int i, pipe[2];
+    int i = 0, p[2], process1 = 0, process2 = 0, returned_proc;
     pid_t pid1, pid2;
 
     if(check_input(input)) {
@@ -61,13 +61,12 @@ void handle_double_input(char input[]) {
         }
 
         if(i == 1) { // No pipe in the command, execute normally
-            printf("Only one command given:\n\t%s", commands[0]);
             /* Create a childprocess and execute the command. */
-            pid = fork();
-            if(pid == 0) {
+            pid1 = fork();
+            if(pid1 == 0) {
                 handle_input(commands[0]);
             }
-            else if(pid < 0) {
+            else if(pid1 < 0) {
                 perror("Failed to fork");
                 exit(1);
             }
@@ -76,10 +75,8 @@ void handle_double_input(char input[]) {
                 wait(0);
         }
         else { // A Pipe in the input, execute both commands
-            printf("Two commands given:\n\t%s\n\t%s", commands[0], commands[1]);
-
             /* Create the pipe. */
-            if(pipe(pipe) < 0) {
+            if(pipe(p) < 0) {
                 perror("Failed to create pipe.");
                 exit(1);
             }
@@ -87,8 +84,8 @@ void handle_double_input(char input[]) {
             /* Create the first childprocess and execute the first command. */
             pid1 = fork();
             if(pid1 == 0) {
-                close(p[0]); // close read
-                dup2(pipe, STDOUT_FILENO); // duplicate pipe and output
+                close(p[0]); // close read end of the pipe
+                dup2(p[1], STDOUT_FILENO); // duplicate pipe and output
                 handle_input(commands[0]);
             }
             else if(pid1 < 0) {
@@ -100,29 +97,35 @@ void handle_double_input(char input[]) {
                 pid2 = fork();
 
                 if(pid2 == 0) {
-                    close(p[1]); // close write
-                    dup2(pipe, STDIN_FILENO); // duplicate pipe and input
+                    close(p[1]); // close write end of the pipe
+                    dup2(p[0], STDIN_FILENO); // duplicate pipe and input
                     handle_input(commands[1]);
                 }
                 else if(pid2 < 0) {
                     perror("Failed to fork");
                     exit(1);
                 }
-
-                /* Parent process wait untill the children are done. */
-                wait(0);
+                else {
+                    /* Parent process wait untill both children are done. */
+                    while(!process1 || !process2) {
+                        if(waitpid(pid1, NULL, WNOHANG) > 0)
+                            process1 = 1;
+                        if(waitpid(pid2, NULL, WNOHANG) > 0)
+                            process2 = 1;
+                    }
+                }
             }
         }
     }
 }
 
 /* Check if the input is a (series of) command(s) */
-int check_input(char[] input) {
+int check_input(char input[]) {
     if (!strcmp(input, "exit\n"))
         /* Command to exit the shell was given. */
         exit(0);
     else if(strlen(input) <= 1) {
-        /* If an empty line or only [enter] was given, do not try to execute. */
+        /* If an empty line or only [enter] was given, do not try to execute.*/
         printf("\n");
         return 0;
     }
