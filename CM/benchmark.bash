56a4073a02ee109688c99cb892dba053cf4cfde4
Taddeus Kroes <taddeuskroes@gmail.com>
2012-10-10 23:17:40 +0200

    Finished benchmark script (do not run without commenting needless lines)

--- CM/benchmark.bash
+++ CM/benchmark.bash
@@ -3,8 +3,26 @@ RUN="sh bin/test_data_structures"
 
 worktime=10
 
-for dtype in {cg,fg,lf}{l,t}; do
-    for nelements in 1000 5000 10000 20000; do
+for i in {1..4}; do
+    for dtype in {cg,fg,lf}{l,t}; do
+        for nelements in 5000 10000 20000 30000 40000; do
+            for nthreads in 1 2 4 6 8 10; do
+                echo -n "$dtype $nthreads $nelements $worktime "
+                runtime="`MEM=1024 $RUN $dtype $nthreads $nelements $worktime \
+                    | grep -E -o \"[[:digit:]]+\"`"
+
+                if [ $runtime ]; then
+                    echo $runtime;
+                else
+                    echo "ERROR"
+                fi
+            done
+        done
+    done
+done
+
+for dtype in {cg,fg,lf}t; do
+    for nelements in {100000..1000000..100000}; do
         for nthreads in 1 2 4 6 8 10; do
             echo -n "$dtype $nthreads $nelements $worktime "
             runtime="`MEM=1024 $RUN $dtype $nthreads $nelements $worktime \
