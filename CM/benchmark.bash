1b9fd463dd71bc0ce77c0977590602639704402d
Taddeus Kroes <taddeuskroes@gmail.com>
2012-10-10 19:09:13 +0200

    Added Python plot script and simplified benchmark script

--- CM/benchmark.bash
+++ CM/benchmark.bash
@@ -1,20 +1,20 @@
 #!/bin/bash
 RUN="sh bin/test_data_structures"
 
-#THREADS="1 `seq 2 2 10`"
-nthreads=4
 worktime=10
 
-for dtype in {cg,fg,lf}l; do
-    for n in 100 1000 5000 `seq 10000 10000 50000`; do
-        echo -n "$dtype $nthreads $n $worktime "
-        MEM=1024 $RUN $dtype $nthreads $n $worktime | egrep -o "[0-9]+"
-    done
-done
+for dtype in {cg,fg,lf}{l,t}; do
+    for nelements in 1000 5000 10000 20000; do
+        for nthreads in 1 2 4 6 8 10; do
+            echo -n "$dtype $nthreads $nelements $worktime "
+            runtime="`MEM=1024 $RUN $dtype $nthreads $nelements $worktime \
+                | grep -E -o \"[[:digit:]]+\"`"
 
-for dtype in {cg,fg,lf}t; do
-    for n in 100 1000 5000 `seq 10000 10000 50000` `seq 100000 100000 1000000`; do
-        echo -n "$dtype $nthreads $n $worktime "
-        MEM=1024 $RUN $dtype $nthreads $n $worktime | egrep -o "[0-9]+"
+            if [ $runtime ]; then
+                echo $runtime;
+            else
+                echo "ERROR"
+            fi
+        done
     done
 done
---
+++ CM/plot.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python
+from __future__ import division
+import sys
+import matplotlib.pyplot as plt
+
+uniq = {}
+
+# Calculate average of duplicates
+for line in sys.stdin.readlines():
+    split = line[:-1].split(' ')
+    dtype = split[0]
+
+    try:
+        nthreads, nelements, worktime, runtime = map(int, split[1:])
+        key = (dtype, nthreads, nelements)
+
+        if key in uniq:
+            oldn, average = uniq[key]
+            newn = oldn + 1
+            uniq[key] = (newn, (oldn * average + runtime) / newn)
+        else:
+            uniq[key] = (1, runtime)
+    except ValueError:
+        pass
+
+# accumulate results in plottable data
+vary_nthreads = {}
+vary_nelements = {}
+
+for (dtype, nthreads, nelements), (_, runtime) in uniq.iteritems():
+    if nelements not in vary_nthreads:
+        vary_nthreads[nelements] = {}
+
+    if dtype not in vary_nthreads[nelements]:
+        vary_nthreads[nelements][dtype] = []
+
+    if nthreads not in vary_nelements:
+        vary_nelements[nthreads] = {}
+
+    if dtype not in vary_nelements[nthreads]:
+        vary_nelements[nthreads][dtype] = []
+
+    vary_nthreads[nelements][dtype].append((nthreads, runtime))
+    vary_nelements[nthreads][dtype].append((nelements, runtime))
+
+# Plot varying number of threads
+for nelements, data in vary_nthreads.iteritems():
+    fig = plt.figure()
+    plt.title('%d elements' % nelements)
+    plt.xlabel('threads')
+
+    for dtype, typedata in data.iteritems():
+        if not typedata:
+            continue
+
+        # Sort data pairs according to horizontal axis
+        nthreads, runtime = zip(*typedata)
+        indices = dict([(n, i) for i, n in enumerate(nthreads)])
+        threads_range = sorted(indices.keys())
+        sorted_runtimes = [runtime[indices[i]] for i in threads_range]
+
+        plt.plot(threads_range, sorted_runtimes, '-o', label=dtype.upper())
+
+    plt.legend()
+
+# Plot varying number of elements
+for nthreads, data in vary_nelements.iteritems():
+    fig = plt.figure()
+    plt.title('%d threads' % nthreads)
+    plt.xlabel('elements')
+
+    for dtype, typedata in data.iteritems():
+        if not typedata:
+            continue
+
+        # Sort data pairs according to horizontal axis
+        nelements, runtime = zip(*typedata)
+        indices = dict([(n, i) for i, n in enumerate(nelements)])
+        elements_range = sorted(indices.keys())
+        sorted_runtimes = [runtime[indices[i]] for i in elements_range]
+
+        plt.plot(elements_range, sorted_runtimes, '-o', label=dtype.upper())
+
+    plt.legend()
+
+plt.show()
