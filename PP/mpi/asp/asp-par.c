65e5149a045287723d0b5d6f1ead2ab037316cf7
Jayke Meijer <jayke.meijer@gmail.com>
2012-11-19 16:35:15 +0100

    Made parallel version, combined all back into one program.

--- PP/mpi/asp/asp-par.c
+++ PP/mpi/asp/asp-par.c
@@ -5,6 +5,117 @@
 #include "mpi.h"
 #include "../table.h"
 
+/******************** Distance calculation *************************/
+double do_dist(int **tab, int n, int myid, int no_proc)
+{
+    int i, j;
+    double total = 0;
+    int lines = n / no_proc;
+    int start = myid * lines;
+    double received_total;
+    MPI_Status stat;
+
+    printf("Going to work. My id=%d, no_proc=%d, n=%d\n", myid, no_proc, n);
+    printf("Lines=%d, start=%d\n", lines, start);
+
+    // Calculate total for my part of the table
+    for(i = start; i < start + lines; i++)
+        for(j = 0; j < n; j++)
+            if(tab[i][j] != MAX_DISTANCE)
+                total += tab[i][j];
+
+    printf("Done calculating. Total of proc %d is %.0lf\n", myid, total);
+
+    if(myid != 0) {
+        // Slave process, send to root.
+        printf("Process %d sending total of %.0lf to master\n", myid, total);
+        MPI_Send(&total, 1, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD);
+    }
+    else {
+        // Root, receive all slave data
+        for(i = 1; i < no_proc; i++) {
+            MPI_Recv(&received_total, 1, MPI_DOUBLE, i, 0, MPI_COMM_WORLD, &stat);
+            printf("Received from %d a total of %.0lf\n", i, received_total);
+            total += received_total;
+        }
+    }
+
+    return total;
+}
+
+/******************** ASP calculation *************************/
+void do_asp(int **tab, int n, int myid, int no_proc)
+{
+    int i, j, k, tmp;
+    int lines = n / no_proc;
+    int start = myid * lines;
+    int other_start;
+    MPI_Status stat;
+
+    // Run the ASP algorithm. Update every pass.
+    for (k = 0; k < n; k++) {
+        // Perform a calculation pass over my part of the table
+        for (i = start; i < start + lines; i++) {
+            if (i != k) {
+                for (j = 0; j < n; j++) {
+                    tmp = tab[i][k] + tab[k][j];
+                    if (tmp < tab[i][j])
+                        tab[i][j] = tmp;
+                }
+            }
+        }
+
+        // Send the just calculated data to the other processes
+        for(i = 0; i < no_proc; i++) {
+            if(i == myid) {
+                // My turn to send
+                for(j = start; j < start + lines; j++)
+                    MPI_Bcast(tab[j], n, MPI_INT, 0, MPI_COMM_WORLD);
+            } else {
+                // Listen and update
+                other_start = i * lines;
+                for(j = other_start; j < other_start + lines; j++) {
+                    MPI_Recv(tab[j], n, MPI_INT, i, 0, MPI_COMM_WORLD, &stat);
+                }
+            }
+        }
+    }
+}
+
+/******************** Diam calculation *************************/
+int do_diam(int **tab, int n, int myid, int no_proc)
+{
+    int i, j, longest = 0;
+    int lines = n / no_proc;
+    int start = myid * lines;
+    int received_longest;
+    MPI_Status stat;
+
+    // Find the longest path in my part of the table
+    for(i = start; i < start + lines; i++)
+        for(j = 0; j < n; j++)
+            if(tab[i][j] != 0 && tab[i][j] > longest)
+                longest = tab[i][j];
+
+    printf("Done searching. Longest of proc %d is %d\n", myid, longest);
+    if(myid != 0) {
+        // Slave process, send to root.
+        printf("Process %d sending total of %d to master\n", myid, longest);
+        MPI_Send(&longest, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
+    }
+    else {
+        // Root, receive all slave data
+        for(i = 1; i < no_proc; i++) {
+            MPI_Recv(&received_longest, 1, MPI_INT, i, 0, MPI_COMM_WORLD, &stat);
+            printf("Received from %d a longest of %d\n", i, received_longest);
+            if(received_longest > longest)
+                longest = received_longest;
+        }
+    }
+
+    return longest;
+}
+
 void usage() {
         printf ("Run the asp program with the following parameters. \n");
         printf (" -read filename :: reads the graph from a file.\n");
@@ -18,13 +129,14 @@ int main ( int argc, char *argv[] ) {
     int id;
     int ierr;
     int p=3;
-    double wtime;
+    double wtime = 0, total;
 
 
-    int n,m, bad_edges=0, oriented=0, lb, ub, i;
+    int n,m, bad_edges=0, oriented=0, i, diameter;
     int **tab;
     int print = 0;
     char FILENAME[100];
+    MPI_Status stat;
 
     /*MPI stuff*/
 
@@ -70,38 +182,67 @@ int main ( int argc, char *argv[] ) {
             init_tab(n,&m,&tab,oriented); // last one = oriented or not ... 
         else
             bad_edges = read_tab(FILENAME, &n, &m, &tab, &oriented);
+
+        fprintf(stderr, "Running program with %d rows and %d edges (%d are bad)\n",
+        n, m, bad_edges);
+
+        printf("The number of processes is %d.\nNow distributing table", p);
+
+        // Distribute the table to all machines.
+        for(i = 0; i < n; i++)
+            MPI_Bcast(tab[i], n, MPI_INT, 0, MPI_COMM_WORLD);
+
+        printf("Table is distributed\n");
+    } else {
+        // Receive the table
+        for(i = 0; i < n; i++)
+            MPI_Recv(tab[i], n, MPI_INT, i, 0, MPI_COMM_WORLD, &stat);
     }
 
-    if ( id == 0 ) {
-        wtime = MPI_Wtime ( );
-
-        printf ( "\n" );
-        printf ( "HELLO_MPI - Master process:\n" );
-        printf ( "  C/MPI version\n" );
-        printf ( "  An MPI example program.\n" );
-        printf ( "\n" );
-        printf ( "  The number of processes is %d.\n", p );
-        printf ( "\n" );
-    }
+    /******************** Distance calculation *************************/
+    if(id == 0)
+        wtime = MPI_Wtime();
 
     MPI_Barrier(MPI_COMM_WORLD);
-    /*
-      Every process prints a hello.
-    */
-    printf ( " %d says: 'Will work on graph!'\n", id);
-    /*
-      Process 0 says goodbye.
-    */
-    if ( id == 0 )
-    {
-        printf ( "\n" );
-        printf ( "HELLO_MPI - Master process:\n" );
-        printf ( "  Normal end of execution: 'Goodbye, world!'\n" );
+    total = do_dist(tab, n, id, p);
 
-        wtime = MPI_Wtime ( ) - wtime;
-        printf ( "\n" );
-        printf ( "  Elapsed wall clock time = %f seconds.\n", wtime );
+    if(id == 0) {
+        wtime = MPI_Wtime() - wtime;
+        fprintf (stderr, "Distance took %f seconds.\n", wtime);
     }
+
+    /******************** ASP *************************/
+    if(id == 0)
+        wtime = MPI_Wtime();
+
+    MPI_Barrier(MPI_COMM_WORLD);
+    do_asp(tab, n, id, p);
+
+    if(id == 0) {
+        wtime = MPI_Wtime() - wtime;
+        fprintf (stderr, "ASP took %f seconds.\n", wtime);
+    }
+
+    /******************** Diameter *************************/
+    if(id == 0)
+        wtime = MPI_Wtime();
+
+    MPI_Barrier(MPI_COMM_WORLD);
+    diameter = do_diam(tab, n, id, p);
+
+    if(id == 0) {
+        wtime = MPI_Wtime() - wtime;
+        fprintf (stderr, "Diameter took %f seconds.\n", wtime);
+    }
+
+    if(id == 0 && print == 1) {
+        printf("Table after ASP:\n");
+        print_tab(tab, n);
+        printf("Total distance: %.0lf\n", total);
+        printf("Diameter: %d\n", diameter);
+    }
+
+
     /*
       Shut down MPI.
     */
--- PP/mpi/asp/asp-seq.c
+++ PP/mpi/asp/asp-seq.c
@@ -1,7 +1,7 @@
 /*
- * asp.c:
+ * asp-seq.c:
  *     All-pairs shortest path implementation based on Floyd's
- *     algorithms.
+ *     algorithms. Also diameter and distance calculation.
  *
  *      Sequential version.
  *
@@ -13,30 +13,49 @@
 #include <sys/time.h>
 #include "../table.h"
 
-/******************** ASP stuff *************************/
+/******************** Distance calculation *************************/
+double do_dist(int **tab, int n)
+{
+    int i, j;
+    double total = 0;
+    for(i = 0; i < n; i++)
+        for(j = 0; j < n; j++)
+            if(tab[i][j] != MAX_DISTANCE)
+                total += tab[i][j];
 
+    return total;
+}
+
+/******************** ASP calculation *************************/
 void do_asp(int **tab, int n)
 {
     int i, j, k, tmp;
 
-    for (k = 0; k < n; k++) {
-        for (i = 0; i < n; i++) {
-            if (i != k) {
+    for (k = 0; k < n; k++)
+        for (i = 0; i < n; i++)
+            if (i != k)
                 for (j = 0; j < n; j++) {
                     tmp = tab[i][k] + tab[k][j];
-                    if (tmp < tab[i][j]) {
+                    if (tmp < tab[i][j])
                         tab[i][j] = tmp;
-                    }
                 }
-            }
-        }
-    }
+}
+
+/******************** Diam calculation *************************/
+int do_diam(int **tab, int n)
+{
+    int i, j, longest = 0;
+    for(i = 0; i < n; i++)
+        for(j = 0; j < n; j++)
+            if(tab[i][j] != 0 && tab[i][j] > longest)
+                longest = tab[i][j];
+
+    return longest;
 }
 
 /******************** Main program *************************/
-
 void usage() {
-    printf ("Run the asp program with the following parameters. \n");
+    printf ("Run the program with the following parameters. \n");
     printf (" -read filename :: reads the graph from a file.\n");
     printf (" -random N 0/1 :: generates a NxN graph, randomly. \n");
     printf ("               :: if 1, the graph is oriented, otherwise it is not"
@@ -46,7 +65,8 @@ void usage() {
 
 int main(int argc, char **argv)
 {
-    int n,m, bad_edges=0, oriented=0, lb, ub, i, total, diameter;
+    int n,m, bad_edges=0, oriented=0, i, diameter;
+    double total;
     int **tab;
     int print = 0;
     struct timeval start;
@@ -79,14 +99,30 @@ int main(int argc, char **argv)
     }
     else bad_edges = read_tab(FILENAME, &n, &m, &tab, &oriented); 
 
-    fprintf(stderr, "Running ASP with %d rows and %d edges (%d are bad)\n", n,
-        m, bad_edges);
-
-    lb = 0;    /* lower bound for rows to be computed */
-    ub = n;    /* upper bound for rows to be computed */
+    fprintf(stderr, "Running program with %d rows and %d edges (%d are bad)\n",
+        n, m, bad_edges);
 
 //    init_tab(n, &tab);
 
+    /******************** Distance calculation *************************/
+    if(gettimeofday(&start, 0) != 0) {
+        fprintf(stderr, "could not do timing\n");
+        exit(1);
+    }
+
+    total = do_dist(tab, n);
+
+    if(gettimeofday(&end, 0) != 0) {
+        fprintf(stderr, "could not do timing\n");
+        exit(1);
+    }
+
+    time = (end.tv_sec + (end.tv_usec / 1000000.0)) - 
+        (start.tv_sec + (start.tv_usec / 1000000.0));
+
+    fprintf(stderr, "Distance calculation took %10.3f seconds\n", time);
+
+    /******************** ASP calculation *************************/
     if(gettimeofday(&start, 0) != 0) {
         fprintf(stderr, "could not do timing\n");
         exit(1);
@@ -104,8 +140,29 @@ int main(int argc, char **argv)
 
     fprintf(stderr, "ASP took %10.3f seconds\n", time);
 
+    /******************** Diam calculation *************************/
+    if(gettimeofday(&start, 0) != 0) {
+        fprintf(stderr, "could not do timing\n");
+        exit(1);
+    }
+
+    diameter = do_diam(tab, n);
+
+    if(gettimeofday(&end, 0) != 0) {
+        fprintf(stderr, "could not do timing\n");
+        exit(1);
+    }
+
+    time = (end.tv_sec + (end.tv_usec / 1000000.0)) - 
+        (start.tv_sec + (start.tv_usec / 1000000.0));
+
+    fprintf(stderr, "Diameter calculation took %10.3f seconds\n", time);
+
     if(print == 1) {
+        printf("Table after ASP:\n");
         print_tab(tab, n);
+        printf("Total distance: %.0lf\n", total);
+        printf("Diameter: %d\n", diameter);
     }
 
     free_tab(tab, n);
